6fbedf6c60ce0afcfd3ce18e8a95a90a2c5ea9f9
thule129 <baekhyunee129@gmail.com>
2025-05-29 02:32:47 +0700

    6

--- routes/web.php
+++ routes/web.php
@@ -76,9 +76,28 @@ Route::redirect('/log-in', '/login');
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
-        Route::get('/dashboard', DashboardAdminController::class);
-        Route::get('/stats', StatsController::class);
+    // Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
+    //     Route::get('/dashboard', DashboardAdminController::class);
+    //     Route::get('/stats', StatsController::class);
+    // });
+
+        Route::prefix('admin')->middleware('is_admin')->group(function(){
+
+        // Tasks inside that /admin group:
+
+
+        // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+        // Put one code line here below
+
+        Route::get('/dashboard',DashboardAdminController::class);
+
+        // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
+        // Put one code line here below
+        Route::get('/stats',StatsController::class);
+
+
+    // End of the /admin Route Group
+
     });
 
         // Tasks inside that /admin group:
