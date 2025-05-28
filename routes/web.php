cb4cee2a8bc555ec2df5ce7a99abc7ef4ee080d2
thule129 <baekhyunee129@gmail.com>
2025-05-29 02:00:36 +0700

    3 attempt

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,10 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/about', 'about')->name('about');
+// Route::view('/about', 'about')->name('about');
+Route::get('/about', function() {
+    return view('about');
+})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
